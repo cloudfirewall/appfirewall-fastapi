7dbbbcd9e3609df1822a811af254fd5b6eaa52a5
spannercode <sandip@sireto.com>
2026-04-23 00:42:05 +0200

    Add PyPi publishing Github Action

--- src/appfirewall_fastapi/__init__.py
+++ src/appfirewall_fastapi/__init__.py
@@ -12,10 +12,15 @@ Public surface::
     appfirewall.record("upload.parse_failed", reason="bad json")
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from ._middleware import AppFirewallMiddleware
 from ._shield import appfirewall
 
-__version__ = "0.1.0"
+try:
+    __version__ = version("appfirewall-fastapi")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
 
 __all__ = [
     "AppFirewallMiddleware",
